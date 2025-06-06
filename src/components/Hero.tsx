40da9ba6484d488dd31e350ecaa98c3739d5f893
sirajkhan8868 <sirajkhan8868447@gmail.com>
2025-06-06 04:33:58 -0600

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,6 @@ const Hero = () => {
           className="w-full h-full object-cover scale-[1.5]"
         >
           <source src={Tailor} type="video/mp4" />
-          Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-black/60 z-10" />
       </div>
