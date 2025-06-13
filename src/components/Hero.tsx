7e42f30d2c6aa7c664c81061b85ef3f236adc912
sirajkhan8868 <sirajkhan8868447@gmail.com>
2025-06-12 23:05:01 -0600

    updateone

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import Tailor from "../assets/videos/Tailor.mp4";
 
 const Hero = () => {
   return (
@@ -16,7 +15,7 @@ const Hero = () => {
           playsInline
           className="w-full h-full object-cover scale-[1.5]"
         >
-          <source src={Tailor} type="video/mp4" />
+          <source src="/videos/Tailor.mp4" type="video/mp4" />
         </video>
         <div className="absolute inset-0 bg-black/60 z-10" />
       </div>
@@ -25,14 +24,19 @@ const Hero = () => {
       <div className="container mx-auto px-4 text-white relative z-20 flex flex-col items-center justify-center text-center">
         <div className="max-w-3xl animate-fade-in">
           <h1 className="text-5xl mb-4 font-serif">Maestro Sarto</h1>
-          <p className="text-lg md:text-xl font-light mb-6 tracking-[0.1rem] leading-relaxed text-tailor-white">
-            Welcome to our boutique tailoring studio in Camps Bay, Cape Town where ocean views meet timeless style. We specialize in bespoke and made-to-measure suits, expertly crafted to reflect your individuality. From precision alterations to unique custom pieces, every garment is tailored to perfection  because you deserve nothing less than excellence.</p>
+         <p className="text-lg md:text-xl font-light mb-6 tracking-[0.1rem] leading-relaxed text-tailor-white text-center">
+  Welcome to our boutique tailoring studio in Camps Bay, Cape Town
+  where ocean views meet timeless style. We specialize in bespoke and
+  made-to-measure suits, expertly crafted to reflect your individuality.
+  From precision alterations to unique custom pieces, every garment is
+  tailored to perfection because you deserve nothing less than excellence.
+</p>
+
           <div className="flex flex-col sm:flex-row gap-4 mt-5 justify-center">
             <Button className="bg-transparent border border-white hover:bg-gray-800 text-white font-medium px-8 py-6 transition-colors duration-300">
               EXPLORE OUR SERVICES
             </Button>
           </div>
-
         </div>
       </div>
 
